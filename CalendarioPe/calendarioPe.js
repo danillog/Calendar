6f2f783a02585b2ff14efaf302f7b7e5e7cd54fa
danillog <danillog821@gmail.com>
2019-10-20 00:55:13 -0400

    1ª tentativa falha de correão de bug na data

--- CalendarioPe/calendarioPe.js
+++ CalendarioPe/calendarioPe.js
@@ -4,29 +4,70 @@ import { format, parse } from 'date-fns';
 import { enUS, eo, pt } from 'date-fns/locale';
 import { addDays, addHours } from 'date-fns/fp';
 import { format, compareAsc } from 'date-fns';
-import startOfToday from 'date-fns/startOfToday'
+import { subMonths, addMonths, parseISO } from 'date-fns';
+import startOfToday from 'date-fns/startOfToday';
 import { faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 import { faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 class calendarioPe  extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dateFixa : new Date(),
+      mes : format( new Date(this.state.dateFixa), 'MMMM',{locale:  require('date-fns/locale/pt')})
+    
+    };
+  }
+
+ 
+ // ano = format(new Date(startOfToday()), 'yyyy')
+
+  alerta = () => {
+    alert( this.state.mes )
+  }
+
+ 
+
+
+  next = () => {
+    let addMes = addMonths((this.state.dateFixa), 1)
+    alert(addMes)
+  }
+  previous = () => {
+    
+  }
+  
   render(){
 
-  
+  /*
     const  br = require('date-fns/locale/pt');
-    const data = {
+    let dateFix = new Date(startOfToday());
+    let data = {
       dia: format(new Date (startOfToday()), 'd'),
-      mes: format(new Date (startOfToday()), 'MMMM',{locale: br}),
+      mes: format((dateFix), 'MMMM',{locale: br}),
       ano: format(new Date(startOfToday()), 'yyyy')
 
     } 
+  
+
+    const week = document.createElement("tr");
+    const day = document.createElement("td");
+    
+    function next(){
+      this.dateFix = subMonths((this.dateFix), 1)
+      this.data.mes = format((this.dateFix), 'MMMM',{locale: br})
+    }
+    function previous(){
+      this.dateFix = subMonths((this.dateFix), 1)
+      this.data.mes = format((this.dateFix), 'MMMM',{locale: br})
+    }
+*/
 
     return (
      /*
-      Duvidas: 
-        Como conectar ambos os conteiners, como mudar a data de ambos os calendarios?
-
+    
       O que fazer ?
         + Construir os dias da semana no calendario, utilizano o js para deixar o codigo menor.
 
@@ -38,13 +79,13 @@ class calendarioPe  extends Component {
             <thead class="thead-dark">
             <tr>
               <th colspan="2" class="botao"> 
-                <button type="button" class="btn btn-outline-primary btn-sm btn-right">
+                <button type="button" class="btn btn-outline-primary btn-sm btn-right" onClick = {this.alerta} >
                  <FontAwesomeIcon icon={faChevronLeft} />
                 </button>
               </th>
-              <th colspan="3" id="data">{data.mes} de {data.ano}</th>
+              <th colspan="3" id="mounthAndYear">{this..mes} de </th>
               <th colspan="2" class="botao">
-                <button type="button" class="btn btn-outline-primary btn-sm btn-left">
+                <button type="button" class="btn btn-outline-primary btn-sm btn-left" onClick ={this.next} >
                  <FontAwesomeIcon icon={faChevronRight} />
                 </button> 
               </th>
@@ -57,7 +98,7 @@ class calendarioPe  extends Component {
               <th scope="col">Sex</th>
               <th scope="col">Sab</th>
             </thead>
-            <tbody>
+            <tbody id="mounth">
               <tr>
                 <td>01</td>
                 <td>02</td>
