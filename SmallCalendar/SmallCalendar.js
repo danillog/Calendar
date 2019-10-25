20be3b95f6f9cd26795291cb9082228eddcd3ea2
danillog <danillog821@gmail.com>
2019-10-25 16:26:53 -0400

    3 files modified

--- SmallCalendar/SmallCalendar.js
+++ SmallCalendar/SmallCalendar.js
@@ -1,116 +1,163 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import { enUS, eo, pt } from 'date-fns/locale';
-import { subMonths, addMonths, format, isBefore, eachDayOfInterval, startOfMonth, endOfMonth, isEqual, isSunday, isSaturday, subDays, addDays  } from 'date-fns';
-import startOfToday from 'date-fns/startOfToday';
-import { faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import { enUS, eo, pt } from "date-fns/locale";
+import {
+  subMonths,
+  addMonths,
+  format,
+  isBefore,
+  eachDayOfInterval,
+  startOfMonth,
+  endOfMonth,
+  isEqual,
+  isSunday,
+  isSaturday,
+  subDays,
+  addDays,
+  parseISO
+} from "date-fns";
+import startOfToday from "date-fns/startOfToday";
+import {
+  faChevronLeft,
+  faChevronRight
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {classNames, createClass } from 'classnames/bind'
 
-class SmallCalendar  extends Component {
-  constructor(props, backend){
-    super(props);    
+
+class SmallCalendar extends Component {
+  
+  constructor(props) {
+    super(props);
     let today = new Date();
-    this.backend = backend;
     this.state = {
       date: today,
-      month : format(today, 'MMMM',{locale: require('date-fns/locale/pt')}),
-      year : format(today, 'yyyy'),
-      calendar : this.buildCalendar(today)
+      month: format(today, "MMMM", { locale: require("date-fns/locale/pt") }),
+      year: format(today, "yyyy"),
+      calendar: this.buildCalendar(today),
+      isActive: 0
     };
   }
 
   previous = () => {
-    let previousDate = subMonths((this.state.date),1);
+    let previousDate = subMonths(this.state.date, 1);
     //A variavel date serve para modificar a semana , pois o mes e ano possuem formatos diferentes de date, estes são strings
     this.setState({
-      date : previousDate,
-      month: format(previousDate, 'MMMM',{locale: require('date-fns/locale/pt')}),
-      year: format(previousDate,'yyyy'),
-      calendar : this.buildCalendar(previousDate)
-    });          
-  }
-
+      date: previousDate,
+      month: format(previousDate, "MMMM", {
+        locale: require("date-fns/locale/pt")
+      }),
+      year: format(previousDate, "yyyy"),
+      calendar: this.buildCalendar(previousDate)
+    });
+  };
 
   next = () => {
-    let nextDate = addMonths((this.state.date),1);
-    //A variavel date serve para modificar a semana , pois o mes e ano possuem formatos diferentes de date, estes são strings  
+    let nextDate = addMonths(this.state.date, 1);
+    //A variavel date serve para modificar a semana , pois o mes e ano possuem formatos diferentes de date, estes são strings
     this.setState({
       date: nextDate,
-      month: format(nextDate, 'MMMM',{locale: require('date-fns/locale/pt')}),
-      year: format(nextDate, 'yyyy'),
+      month: format(nextDate, "MMMM", {
+      locale: require("date-fns/locale/pt")
+      }),
+      year: format(nextDate, "yyyy"),
       calendar: this.buildCalendar(nextDate)
-    });   
-  }
+    });
+  };
 
-  gotoDate = ev => {
-    alert('voce clicou na data: ' + ev.currentTarget.dataset.iterator);
-  }
-
-  buildCalendar = (today) => {
-    let currentDate = today
+  activeFocus = ev => {  
+    console.log(ev.currentTarget.dataset.iterator);
     
-    let startWeek = startOfMonth(currentDate)
+    let clickDate = parseISO(ev.currentTarget.dataset.iterator);
+    console.log(clickDate);   
+    this.setState({
+      date: clickDate,
+      calendar: this.buildCalendar(clickDate)
+    });
+    console.log('fim');
+    //alert("voce clicou na data: " + ev.currentTarget.dataset.iterator); className= {'active'}
+//    ev.currentTarget.dataset.iterator    
+  };
 
-    while(!isSunday(startWeek)){
-      startWeek = subDays(startWeek,1);
-    }
+  onMonthChanged = ev => {
+
+  }
   
+  buildCalendar = referenceDate => {
+    let currentDate = referenceDate;
+    
+
+    let startWeek = startOfMonth(currentDate);
+
+    while (!isSunday(startWeek)) {
+      startWeek = subDays(startWeek, 1);
+    }
+
     let endWeek = endOfMonth(currentDate);
 
-    while(!isSaturday(endWeek)){      
+    while (!isSaturday(endWeek)) {
       endWeek = addDays(endWeek, 1);
     }
 
     let nextMonth = addDays(endWeek, 1);
-
     let calendar = [];
     let iterator = startWeek;
     let key = 0;
-    while(isBefore(iterator, endWeek)){
-      let week = [];      
-      for(let i = 0; i < 7 && isBefore(iterator, nextMonth); i++){        
-        week.push(<td data-iterator={iterator} onClick={this.gotoDate}>{format(iterator, 'dd')}</td>);        
+    while (isBefore(iterator, endWeek)) {
+      let week = [];
+      for (let i = 0; i < 7 && isBefore(iterator, nextMonth); i++) {
+        week.push(
+          <td data-iterator={format(iterator, 'yyy-MM-dd') + 'T00:00:00'}  onClick={this.activeFocus} className={ iterator.getDate() == currentDate.getDate() ? 'active': ''}>
+            {format(iterator, "dd")}
+          </td>
+        );
         iterator = addDays(iterator, 1);
-      }      
+      }
       calendar.push(<tr key={key++}>{week}</tr>);
     }
 
-    return calendar;  
- }
-  
-  render(){
+    return calendar;
+  };
 
+  render() {
     return (
-
-          <table class="table ">
-            <thead class="thead-dark">
-            <tr>
-              <th colspan="2" class="botao"> 
-                <button type="button" class="btn btn-outline-primary btn-sm btn-right" onClick = {this.previous} >
-                 <FontAwesomeIcon icon={faChevronLeft} />
-                </button>
-              </th>
-              <th colspan="3" id="mounthAndYear">  {this.state.month} de {this.state.year} </th>
-              <th colspan="2" class="botao">
-                <button type="button" class="btn btn-outline-primary btn-sm btn-left" onClick ={this.next} >
-                 <FontAwesomeIcon icon={faChevronRight} />
-                </button> 
-              </th>
-            </tr>
-              <th scope="col">Dom</th>
-              <th scope="col">Seg</th>
-              <th scope="col">Ter</th>
-              <th scope="col">Qua</th>
-              <th scope="col">Qui</th>
-              <th scope="col">Sex</th>
-              <th scope="col">Sab</th>
-            </thead>
-            <tbody id="mounth">    
-            {  this.state.calendar }
-            </tbody>
-          </table>           
-            
+      <table class="table ">
+        <thead class="thead-dark">
+          <tr>
+            <th colspan="2" class="botao">
+              <button
+                type="button"
+                class="btn btn-outline-primary btn-sm btn-right"
+                onClick={this.previous}
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+            </th>
+            <th colspan="3" id="mounthAndYear" onclick={this.onMonthChanged}>
+              {" "}
+              {this.state.month} de {this.state.year}{" "}
+            </th>
+            <th colspan="2" class="botao">
+              <button
+                type="button"
+                class="btn btn-outline-primary btn-sm btn-left"
+                onClick={this.next}
+              >
+                <FontAwesomeIcon icon={faChevronRight} />
+              </button>
+            </th>
+          </tr>
+          <th scope="col">Dom</th>
+          <th scope="col">Seg</th>
+          <th scope="col">Ter</th>
+          <th scope="col">Qua</th>
+          <th scope="col">Qui</th>
+          <th scope="col">Sex</th>
+          <th scope="col">Sab</th>
+        </thead>
+        <tbody id="mounth">{this.state.calendar}</tbody>
+      </table>
     );
   }
 }
